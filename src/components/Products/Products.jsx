ceefdf7d7198b8b66410dfc93c80684b42a1dad1
danial alijanianzadeh <88320221+danialajz82@users.noreply.github.com>
2023-12-21 12:05:16 +0330

    nothing

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,10 +1,13 @@
- import React from 'react'
- import css from "./Products.module.css"
- 
- const Products = () => {
-   return (
-     <div>Products</div>
-   )
- }
- 
- export default Products
+import React from "react";
+import css from "./Products.module.css";
+import Plane from "../../assets/plane.png";
+const Products = () => {
+  return (
+    <div className={css.container}>
+      <img src={Plane} alt="" />
+      <h1>Our Featured Product</h1> 
+    </div>
+  );
+};
+
+export default Products;
